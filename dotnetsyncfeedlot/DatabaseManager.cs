e886fb497c129c5fb3ee937c8855253b2f772f71
MasroorSyed-Tag <masroor.syed@telusagriculture.com>
2022-06-14 09:15:42 -0600

    commit

--- dotnetsyncfeedlot/DatabaseManager.cs
+++ dotnetsyncfeedlot/DatabaseManager.cs
@@ -1,6 +1,75 @@
+using Couchbase.Lite;
+using Couchbase.Lite.DI;
+using Couchbase.Lite.Sync;
+
 namespace dotnetsyncfeedlot;
 
 public class DatabaseManager
 {
+    private string _databaseName = "feedlot-test";
+    private Replicator? _replicator;
+    private ListenerToken _replicatorListenerToken;
+    private bool _replicationComplete = false;
+    private readonly Uri _remoteSyncUrl = new Uri("ws://localhost:4984/feedlot-test");
+    public Database GetDatabase()
+    {
+        var defaultDirectory = Service.GetInstance<IDefaultDirectoryResolver>().DefaultDirectory();
+        var databaseConfig = new DatabaseConfiguration
+        {
+            Directory = defaultDirectory
+        };
+        return new Database(_databaseName, databaseConfig);
+    }
     
+    public void StartReplicationAsync()
+        // end::startSync[]
+    {
+        var database = GetDatabase();
+        var targetUrlEndpoint = new URLEndpoint(new Uri(_remoteSyncUrl, _databaseName));
+        // tag::replicationconfig[]
+        var configuration = new ReplicatorConfiguration(database, targetUrlEndpoint) // <1>
+        {
+            
+            Authenticator = new BasicAuthenticator("alice", "password"),
+            ReplicatorType = ReplicatorType.Pull,
+            Channels = new [] {"ref-data"},
+            Continuous = false, // <3>
+        };
+        // end::replicationconfig[]
+        // tag::replicationinit[]
+        _replicator = new Replicator(configuration);
+        // end::replicationinit[]
+        // tag::replicationlistener[]
+        _replicatorListenerToken = _replicator.AddChangeListener(OnReplicatorUpdate);
+        // end::replicationlistener[]
+        // tag::replicationstart[]
+        _replicator.Start();
+        // end::replicationstart[]
+    }
+    private void OnReplicatorUpdate(object sender, ReplicatorStatusChangedEventArgs e)
+    {
+        var status = e.Status;
+        switch (status.Activity)
+        {
+            case ReplicatorActivityLevel.Busy:
+                break;
+            case ReplicatorActivityLevel.Connecting:
+                Console.WriteLine("Connecting to Sync Gateway.");
+                break;
+            case ReplicatorActivityLevel.Idle:
+                break;
+            case ReplicatorActivityLevel.Offline:
+                break;
+            case ReplicatorActivityLevel.Stopped:
+                _replicationComplete = true;
+                Console.WriteLine("Completed syncing documents.");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+    public bool GetStatus()
+    {
+        return _replicationComplete;
+    }
 }
--- dotnetsyncfeedlot/Program.cs
+++ dotnetsyncfeedlot/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Couchbase.Lite.Query;
+using dotnetsyncfeedlot;
+
+var dbManager = new DatabaseManager();
+var database = dbManager.GetDatabase();
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+dbManager.StartReplicationAsync();
+while (!dbManager.GetStatus())
+{
+}
+var query = QueryBuilder.Select(SelectResult.All())
+    .From(DataSource.Database(database));
+// Run the query
+var result = query.Execute(); 
+Console.WriteLine($"Number of rows :: {result.AllResults().Count}");
+watch.Stop();
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
